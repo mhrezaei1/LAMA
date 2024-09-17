20a64ae3dea4c1a9c3796c66c7a92b174f1619b0
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2024-09-16 23:31:14 -0700

    try

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -222,11 +222,11 @@ if __name__ == "__main__":
 
     print("2. T-REx")
     parameters = get_TREx_parameters()
-    run_all_LMs(parameters)
+    # run_all_LMs(parameters)
 
     print("3. ConceptNet")
     parameters = get_ConceptNet_parameters()
-    # run_all_LMs(parameters)
+    run_all_LMs(parameters)
 
     print("4. SQuAD")
     parameters = get_Squad_parameters()
